9e87e402ac67ef11a9cd1f496d3abd64c3c9bc56
Fedor <fedor.kochkin48@gmail.com>
2022-12-31 01:00:49 +0300

    решение задачи 66

---
+++ zadacha66/Program.cs
@@ -0,0 +1,19 @@
+﻿int Summa( int a, int b, ref int sum)
+{
+    if(a <= b) {
+    sum = sum + a;
+ return Summa( a + 1, b , ref sum);
+    }
+    return sum;
+}
+
+Console.Clear();
+Console.WriteLine("введите первое число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("введите второе число");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Сумма чисел между введенными равна: ");
+int sum = 0;
+Console.WriteLine(Summa( a, b, ref sum));
+
+
