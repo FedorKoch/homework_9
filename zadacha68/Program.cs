ba6b9f4d37bb7b5f7a462a99ca1016871595b019
Fedor <fedor.kochkin48@gmail.com>
2022-12-31 01:06:31 +0300

    решение задачи 68

---
+++ zadacha68/Program.cs
@@ -0,0 +1,16 @@
+﻿int Accerman(int n, int m)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return Accerman(n - 1, 1);
+    else
+      return Accerman(n - 1, Accerman(n, m - 1));
+}
+Console.Clear();
+Console.WriteLine("введите первое число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("введите второе число");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine(Accerman( a, b));
